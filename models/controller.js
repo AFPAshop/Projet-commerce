a66f82fa371bed5d501dea8e444021a292525348
Eitheladan <eithel57@gmail.com>
2021-12-03 11:57:03 +0100

    ajout

---
+++ models/controller.js
@@ -0,0 +1,11 @@
+const conn = require('./mysqlconfig.js');
+
+exports.getAll = function (table, callback) {
+    var sql = "SELECT * FROM " + table;
+    conn.query(sql, function (error, rows) {
+        if (error) {
+            console.log(error);
+        }
+        callback(rows);
+    })
+}
