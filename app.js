4115af87ac3c84dcb19d575a19e40eec16b3cb6a
Eitheldan <eithel57@gmail.com>
2021-12-06 16:31:08 +0100

    ajout

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const bdd = require('./models/controller');
 
 const app = express();
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({
+    extended: true
+}));
 
 app.set('views', __dirname + '/views'); // répertoire contenant les templates
 // déclaration du moteur de template utilisé
@@ -19,12 +21,18 @@ app.get('/', function (req, res) {
 
 
 app.get('/route1', function (req, res) {
-    res.render('data1', { resultat: "Félicitation !!!!" })
+    res.render('data1', {
+        resultat: "Félicitation !!!!"
+    })
 })
 
 app.get('/route2/:param1/:param2', function (req, res) {
     console.log(req.params);
-    res.render('data2', { resultat: "Félicitation !!!!", p1: req.params.param1, p2: req.params.param2 });
+    res.render('data2', {
+        resultat: "Félicitation !!!!",
+        p1: req.params.param1,
+        p2: req.params.param2
+    });
 })
 
 app.get('/route3', function (req, res) {
@@ -45,17 +53,53 @@ app.get('/route4', function (req, res) {
 app.get('/route5', function (req, res) {
     bdd.getAll("articles", function (articles) {
         console.log(articles);
-        res.render("data4", { articles: articles });
+        res.render("data4", {
+            articles: articles
+        });
     })
 })
-
 
 app.get('/route6', function (req, res) {
     res.render("creart");
 })
 
-app.post('/addarticle', function (req, res) {
+app.post('/add_article', function (req, res) {
     console.log(req.body);
+    bdd.createarticles('articles', req.body, function () {
+        res.redirect('/route5');
+    });
 })
+
+app.get('/modif_article/:id', function (req, res) {
+    bdd.getOne('articles', req.params.id, function (article) {
+        console.log(article);
+        res.render("article", {
+            article: article
+        });
+
+    });
+
+})
+
+app.post('/update_article/:id', function (req, res) {
+    console.log(req.body);
+    bdd.updateArticle('articles', req.params.id, req.body, function () {
+        res.redirect('/route5');
+    });
+})
+
+app.get('/stocks', function (req, res) {
+    bdd.getAll("articles", function (articles) {
+        console.log(articles);
+        res.render("stock", {
+            articles: articles
+        });
+    })
+})
+
+app.use(function (req, res, next) {
+    console.log('une requete a été effectué a cette heure : ', Date.now());
+    next();
+});
 
 app.listen(8082);
--- models/controller.js
+++ models/controller.js
@@ -9,3 +9,37 @@ exports.getAll = function (table, callback) {
         callback(rows);
     })
 }
+
+exports.createarticles = function (table, datas, callback) {
+    var sql = "INSERT INTO " + table + " VALUES(NULL,'" + datas.nom + "','" + datas.prix + "','" + datas.quantite + "'); ";
+    console.log(sql);
+    conn.query(sql, function (error) {
+        if (error) {
+            console.log(error);
+        }
+        callback();
+    })
+}
+
+exports.getOne = function (table, id, callback) {
+    var sql = "SELECT * FROM " + table + " WHERE id=" + id;
+    console.log(sql);
+    conn.query(sql, function (error, rows) {
+        if (error) {
+            console.log(error);
+        }
+        callback(rows);
+    })
+}
+
+exports.updateArticle = function (table, id, datas, callback) {
+    var sql = "UPDATE " + table + " SET nom ='" + datas.nom + "',prix ='" + datas.prix + "',quantite ='" + datas.quantite + "' WHERE id ='" + id + "'";
+    console.log(sql);
+    conn.query(sql, function (error) {
+        if (error) {
+            console.log(error);
+        }
+        callback();
+    })
+
+}
